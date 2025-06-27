e590fc84a49377367cf2c0175afd5446015ae837
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2025-06-27 10:27:44 +0000

    aula 27-06

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,21 +3,11 @@ import eevee from './assets/eevee.jpg';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import './Home.css'; // Vamos usar isso para o fundo vermelho
 
-const images = [poke, eevee, poke, eevee, poke, eevee, poke, eevee, poke, eevee, poke, eevee, poke, eevee];
-
 const Home = () => {
   return (
-    <div className="home-background">
-      <Container className="py-4">
-        <Row className="g-3 justify-content-center">
-          {images.map((imgSrc, index) => (
-            <Col key={index} xs={6} sm={4} md={3} lg={2} className="d-flex justify-content-center">
-              <Image src={imgSrc} alt={`pokemon-${index}`} fluid rounded />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </div>
+      <div className="Home">
+          <h1>Home</h1>
+      </div>
   );
 };
 
