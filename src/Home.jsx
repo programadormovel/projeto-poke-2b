c7d7a43cb5688ef12c6b771c6abd808a72db43fd
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2025-06-27 10:20:19 +0000

    layout

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,24 @@
-import poke from "./assets/pokeapi_256.png"
-import eevee from './assets/eevee.jpg'
-import './Home.css'
+import poke from "./assets/pokeapi_256.png";
+import eevee from './assets/eevee.jpg';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import './Home.css'; // Vamos usar isso para o fundo vermelho
 
-const Home = ()=> {
-    return (
-        <div className="pokeflex">
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-        </div>
-    )
-}
+const images = [poke, eevee, poke, eevee, poke, eevee, poke, eevee, poke, eevee, poke, eevee, poke, eevee];
 
-export default Home
+const Home = () => {
+  return (
+    <div className="home-background">
+      <Container className="py-4">
+        <Row className="g-3 justify-content-center">
+          {images.map((imgSrc, index) => (
+            <Col key={index} xs={6} sm={4} md={3} lg={2} className="d-flex justify-content-center">
+              <Image src={imgSrc} alt={`pokemon-${index}`} fluid rounded />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
+export default Home;
