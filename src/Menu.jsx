ed0a0bfa935c90639cd95fe5f2c80d12c2c78ecc
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2025-06-27 11:20:10 +0000

    menu

---
+++ src/Menu.jsx
@@ -0,0 +1,22 @@
+const Menu = () => {
+    return (
+        <div className="Menu">
+            <ul className="nav">
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Item do Menu</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/home">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/pokeflex">PokeFlex</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/pokegrid">PokeGrid</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu
